f56f293243ebdaa6e32716da8cee1e5503e82864
jstevenson02 <jacobryanstevenson@gmail.com>
2023-06-12 13:17:14 -0500

    adminpanel init

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,10 +2,10 @@ const Navbar = () => {
   return (
     <>
       <div className="bg-purple-500 p-4 sticky top-0">
-        <div className="container">
+        <div className="flex flex-row gap-6 ">
           <a href="/">Home</a>
           <a href="/products">Products</a>
-          <a>Products</a>
+          <a href="/signin">Sign In</a>
         </div>
       </div>
     </>
