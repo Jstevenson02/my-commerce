b4532b90806d53848d2e9183c1b33693bed769a3
jstevenson02 <jacobryanstevenson@gmail.com>
2023-06-12 13:43:22 -0500

    setup oauth login

--- admin-panel/pages/index.js
+++ admin-panel/pages/index.js
@@ -1,7 +1,24 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Home() {
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-purple-500 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            className="bg-white text-black rounded-lg p-3"
+            onClick={() => signIn("google")}
+          >
+            Sign with Google
+          </button>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="bg-purple-500 w-screen h-screen">
+      <div>Logged In {session.user.email}</div>
     </div>
   );
 }
